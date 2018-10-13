cc68a1ef11160216a525cbf371be6e651700941d
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-10-14 00:02:48 +0900

    空白調整

--- script/vagrant/retake.sh
+++ script/vagrant/retake.sh
@@ -4,22 +4,22 @@
 # set -x
 
 echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m vagrant halt                      \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+vagrant halt
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m vagrant destroy -f                \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+vagrant destroy -f
+
+echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m remove vagrant                    \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 rm -rf .vagrant
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m vagrant halt \e[m'
-echo -e $'\e[1;31m ================================= \e[m'
-vagrant halt
-
-echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m vagrant destroy -f \e[m'
-echo -e $'\e[1;31m ================================= \e[m'
-vagrant destroy -f
-
-echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m vagrant up --provision \e[m'
+echo -e $'\e[1;31m vagrant up                        \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 vagrant up
 

a07946f018b757fe6fcb6ce4cd63fff568ece628
Toshifumi Kurosawa <bizp96@gmail.com>
2018-10-17 10:27:25 +0900

    docker_fileに修正

--- docker/script/init_mysql.sh
+++ docker/script/init_mysql.sh
@@ -18,8 +18,8 @@ echo -e $'\e[1;31m workspaceの作り直し \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 sudo rm -rf workspace/
 mkdir workspace/
-cp -rf docker/web/ruby/rails/Gemfile workspace/Gemfile
-cp -rf docker/web/ruby/rails/Gemfile.lock workspace/Gemfile.lock
+cp -rf docker_file/web/ruby/rails/Gemfile workspace/Gemfile
+cp -rf docker_file/web/ruby/rails/Gemfile.lock workspace/Gemfile.lock
 
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m rails new \e[m'
@@ -35,7 +35,7 @@ sudo chown -R $USER:$USER workspace/
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m rake db:create \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-cp -f docker/database/mysql/database.yml workspace/config/database.yml
+cp -f docker_file/database/mysql/database.yml workspace/config/database.yml
 
 docker-compose up -d mysql
 docker-compose run web bundle exec rake db:create

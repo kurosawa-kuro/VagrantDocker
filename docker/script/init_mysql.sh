c1feedc6e7bace8897de10da121171efb1dcb33d
Toshifumi Kurosawa <bizp96@gmail.com>
2018-10-17 11:12:43 +0900

    進捗文言修正

--- docker/script/init_mysql.sh
+++ docker/script/init_mysql.sh
@@ -4,17 +4,17 @@
 # set -x
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m docker-compose down -v \e[m'
+echo -e $'\e[1;31m コンテナ ボリュームの削除             \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 docker-compose down -v
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m コンテナの停止と削除 \e[m'
+echo -e $'\e[1;31m コンテナの停止と削除                 \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m workspaceの作り直し \e[m'
+echo -e $'\e[1;31m workspaceの作り直し                 \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 sudo rm -rf workspace/
 mkdir workspace/
@@ -22,18 +22,18 @@ cp -rf docker_file/web/ruby/rails/Gemfile workspace/Gemfile
 cp -rf docker_file/web/ruby/rails/Gemfile.lock workspace/Gemfile.lock
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m rails new \e[m'
+echo -e $'\e[1;31m コンテナ作成、Rails新規プロジェクト作成 \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 docker-compose run web rails new . --force --database=mysql
 docker-compose build web
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m chown $USER workspace \e[m'
+echo -e $'\e[1;31m workspaceの権限をrootからuserに変更  \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 sudo chown -R $USER:$USER workspace/
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m rake db:create \e[m'
+echo -e $'\e[1;31m データベース作成                     \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
 cp -f docker_file/database/mysql/database.yml workspace/config/database.yml
 
@@ -41,10 +41,9 @@ docker-compose up -d mysql
 docker-compose run web bundle exec rake db:create
 
 echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m コンテナ起動 \e[m'
+echo -e $'\e[1;31m コンテナ起動、ウェブサーバー起動       \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-# rm -f workspace/tmp/pids/server.pid && docker-compose up web mysql mysql-gui portainer
-rm -f workspace/tmp/pids/server.pid && docker-compose up web mysql mysql-gui portainer cloud9
+docker-compose up web mysql mysql-gui portainer cloud9
 
 # デバッグを終了する
 set +x

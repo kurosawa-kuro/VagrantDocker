396a4f42e05c73c0ac84e0aadafb378ee23bbb60
Toshifumi Kurosawa <kuromailserver@gmail.com>
2018-10-18 12:17:27 +0900

    Update docker.sh

--- vagrant/bootstrap/docker.sh
+++ vagrant/bootstrap/docker.sh
@@ -6,14 +6,21 @@
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m = install docker                = \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-curl -fsSL get.docker.com | CHANNEL=stable sh
-usermod -aG docker vagrant
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update -y
+sudo apt install -y docker-ce
 
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m = install docker-compose        = \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m = docker version                = \e[m'

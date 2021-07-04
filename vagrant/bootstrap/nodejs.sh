a528910d79b6da9bdf563d7f129efda399a933a8
John Doe <johndoe@example.com>
2021-07-04 16:55:10 +0900

    node.js

--- vagrant/bootstrap/nodejs.sh
+++ vagrant/bootstrap/nodejs.sh
@@ -4,34 +4,36 @@
 # set -x
 
 echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m = install nvm                   = \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+sudo apt-get install curl
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+echo -e $'\e[1;31m ================================= \e[m'
+echo -e $'\e[1;31m = check nvm                     = \e[m'
+echo -e $'\e[1;31m ================================= \e[m'
+command -v nvm
+
+echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m = install node.js               = \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-apt-get install -y nodejs npm
-
-npm cache clean
-npm install n -g
-
-n stable
-ln -sf /usr/local/bin/node /usr/bin/node
+nvm install --lts
 
 echo -e $'\e[1;31m ================================= \e[m'
 echo -e $'\e[1;31m = install node.js               = \e[m'
 echo -e $'\e[1;31m ================================= \e[m'
-node -v
+node --version
 
-echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m = purge -y nodejs npm           = \e[m'
-echo -e $'\e[1;31m ================================= \e[m'
-apt-get purge -y nodejs npm
+# echo -e $'\e[1;31m ================================= \e[m'
+# echo -e $'\e[1;31m = install typescript            = \e[m'
+# echo -e $'\e[1;31m ================================= \e[m'
+# npm install -g typescript
 
-echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m = install typescript            = \e[m'
-echo -e $'\e[1;31m ================================= \e[m'
-npm install -g typescript
+# echo -e $'\e[1;31m ================================= \e[m'
+# echo -e $'\e[1;31m = install yarn                  = \e[m'
+# echo -e $'\e[1;31m ================================= \e[m'
+# npm install -g yarn
 
-echo -e $'\e[1;31m ================================= \e[m'
-echo -e $'\e[1;31m = install yarn                  = \e[m'
-echo -e $'\e[1;31m ================================= \e[m'
-npm install -g yarn
 # デバッグを終了する
 set +x
